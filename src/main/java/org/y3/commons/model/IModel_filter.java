5efa0b605633456e85f8e1ead1e000f2e5e85dc4
ryppo <ryppo@it-happens.de>
2015-01-23 14:02:36 +0100

    mod: improve to fail

---
+++ src/main/java/org/y3/commons/model/IModel_filter.java
@@ -0,0 +1,12 @@
+package org.y3.commons.model;
+
+/** 
+ * <p>Title: org.y3.commons.model - IModel_filter</p>
+ * <p>Description: Model filter interface</p>
+ * <p>Copyright: 2015</p>
+ * <p>Organisation: IT-Happens.de</p>
+ * @author Christian.Rybotycky
+*/
+public interface IModel_filter {
+
+}
